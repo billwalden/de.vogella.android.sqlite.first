c8a950b9991a207a14ed0dc262fc15f0e449e429
billwalden <wewald02@louisville.edu>
2016-08-10 16:56:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/igear/devogellaandroidsqlitefirst/Comment.java
@@ -0,0 +1,30 @@
+package com.example.igear.devogellaandroidsqlitefirst;
+
+/**
+ * Created by IGear on 8/10/2016.
+ */
+public class Comment {
+    private long id;
+    private String comment;
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public String getComment(){
+        return comment;
+    }
+
+    public void setComment(String comment){
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString(){
+        return comment;
+    }
+}
